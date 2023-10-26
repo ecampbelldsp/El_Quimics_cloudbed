6ca0d585c75f1e476d4ef0ce52fc113996be59cb
RobertoNovo <44671643+RobertoNovo@users.noreply.github.com>
2023-10-26 11:25:11 +0200

    Update email server

--- email_server/send.py
+++ email_server/send.py
@@ -45,19 +45,22 @@ def oauth_init():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('credentials/token.json'):
-        creds = Credentials.from_authorized_user_file('credentials/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('credentials/token.json', 'w') as token:
-            token.write(creds.to_json())
+    try:
+        if os.path.exists('credentials/token.json'):
+            creds = Credentials.from_authorized_user_file('credentials/token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials/credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('credentials/token.json', 'w') as token:
+                token.write(creds.to_json())
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
     return creds
 def gmail_send_message(FROM = None, TO = None, attachment_filename = None, message_text="", subject = 'Automated draft'):
     """Create and send an email message
